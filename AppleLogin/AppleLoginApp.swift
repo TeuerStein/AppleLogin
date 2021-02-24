d92a7314e778211198e0df288225cb421c4e71d5
TeuerStein <xenkai123456789@gmail.com>
2021-02-24 20:53:08 +0100

    Initial commit

---
+++ AppleLogin/AppleLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLoginApp.swift
+//  AppleLogin
+//
+//  Created by OLEKSANDR SOKOLOV on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
