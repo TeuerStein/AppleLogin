3614d047385d78ba0bcac158a2c93c6e28eddbca
TeuerStein <xenkai123456789@gmail.com>
2021-02-27 23:08:38 +0100

    Was created base view for table of videos

--- PlayerTransition/View/VideoCardView.swift
+++ PlayerTransition/View/VideoCardView.swift
@@ -10,11 +10,35 @@ import SwiftUI
 struct VideoCardView: View {
     var video: Video
     var body: some View {
-        VStack(spacing: 10) {
+        VStack(spacing: -30) {
             Image(video.image)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(height: 250)
+            
+            HStack(spacing: 15) {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40)
+                
+                VStack(alignment: .leading, spacing: 6) {
+                    Text(video.title)
+                        .fontWeight(.semibold)
+                        .font(.callout)
+                    
+                    HStack {
+                        Text("Teuer Stein")
+                            .fontWeight(.bold)
+                            .font(.caption)
+                        
+                        Text(".12K Views. 5 Days Ago")
+                            .font(.caption)
+                    }
+                    .foregroundColor(.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
         }
         .padding(.horizontal)
     }
